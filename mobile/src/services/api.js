a8228f256f9f6b542933cdf00a1d5fea3bbb8356
NOF03 <105490848+NOF03@users.noreply.github.com>
2023-12-14 18:59:39 +0000

    api bug

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export const apiURL= "https://1eff-193-136-232-254.ngrok-free.app";
-=======
-export const apiURL= "https://bf9b-193-136-232-254.ngrok-free.app";
->>>>>>> 13bd8b2bc9b4b588742563749355743e180ba6a7
